1df38add90b8f83fbd4c7af4f8cf77b498b6d02b
Nahida Akther <ummenahida20@gmail.com>
2025-10-28 01:18:10 +0600

    create payment controller

---
+++ src/app/modules/payment/payment.route.ts
@@ -0,0 +1,10 @@
+
+
+// import express from "express";
+// import { PaymentController } from "./payment.controller";
+
+// const router = express.Router();
+
+// router.post("/",PaymentController.handleStripeWebhookEvent)
+
+// export const paymentRouter = router;
