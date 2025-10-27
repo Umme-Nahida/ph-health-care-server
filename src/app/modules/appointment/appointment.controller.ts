aee122fb4371f9db275b1643b6a2475563bd3f74
Nahida Akther <ummenahida20@gmail.com>
2025-10-28 01:18:41 +0600

    add appointment controller

--- src/app/modules/appointment/appointment.controller.ts
+++ src/app/modules/appointment/appointment.controller.ts
@@ -3,6 +3,7 @@ import catchAsync from "../../shared/catchAsync";
 import { AppointmentService } from "./appointment.service";
 import sendResponse from "../../shared/sendResponse";
 import { IJWTPayload } from "../../Types/types";
+import { pick } from "../../helpers/pick";
 
 
 const createAppointment = catchAsync(async (req: Request & { user?: IJWTPayload }, res: Response) => {
@@ -18,6 +19,24 @@ const createAppointment = catchAsync(async (req: Request & { user?: IJWTPayload 
     })
 });
 
+
+const myAppointment = catchAsync(async (req: Request & { user?: IJWTPayload }, res: Response) => {
+    const user = req.user;
+    console.log("req", user)
+    const options = pick(req.query, ["page", "limit", "sortBy", "orderBy"])
+    const fillters = pick(req.query, ["status", "paymentStatus"])
+
+    const result = await AppointmentService.myAppointment(user as IJWTPayload, fillters, options);
+
+    sendResponse(res, {
+        statusCode: 201,
+        success: true,
+        message: "My appointment retrieve successfully!",
+        data: result
+    })
+});
+
 export const AppointmentController = {
     createAppointment,
+    myAppointment
 }
